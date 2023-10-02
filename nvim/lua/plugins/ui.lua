aa7153039f62b5142b91f66dc8e902194ae6830a
WizardStark <juan.barnard10@gmail.com>
2023-10-02 08:21:28 +0200

    Update nvim-tree default width

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -54,7 +54,7 @@ return {
             require("nvim-tree").setup({
                 sort_by = "case_sensitive",
                 view = {
-                    width = 30,
+                    width = 40,
                 },
                 renderer = {
                     group_empty = true,
