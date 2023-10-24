1a5e8e4171a120b8c23b2142bab42132612955db
WizardStark <juan.barnard10@gmail.com>
2023-10-24 19:12:14 +0200

    Add additional dependencies to setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt update && sudo apt upgrade -y
-sudo apt-get -y install ninja-build gettext cmake unzip curl wget nodejs npm tmux
+sudo apt-get -y install ninja-build gettext cmake unzip curl wget nodejs npm tmux fd-find ripgrep
 
 sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.5/zsh-in-docker.sh)" -- \
     -t "jonathan" \
@@ -18,6 +18,11 @@ rm -rf neovim
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
 git clone https://github.com/WizardStark/dotfiles
 cd dotfiles
 sh sync.sh nvim r
@@ -28,5 +33,5 @@ source ~/.zshrc
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 tmux new-session -d -s setup_session 'tmux source ~/.config/tmux/tmux.conf'
-
-echo "Attach to a new tmux session and type Ctrl+A Shift+I to install plugins"
+chmod +x ~/.config/tmux/plugins/tpm/scripts/install_plugins.sh
+sh ~/.config/tmux/plugins/tpm/scripts/install_plugins.sh
