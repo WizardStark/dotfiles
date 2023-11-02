d1351b00229eb3b635eab4f5b590b6403db17c51
WizardStark <juan.barnard10@gmail.com>
2023-11-02 10:48:26 +0200

    Remove fallback on lsp for autoformatting and autoformat all config files

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,206 +1,206 @@
 return {
-    --git
-    {
-        'lewis6991/gitsigns.nvim',
-        event = 'VeryLazy',
-        opts = {
-            current_line_blame = true,
-            current_line_blame_opts = {
-                virt_text_pos = 'right_align',
-                delay = 500
-            },
-        }
-    },
-    --treesitter
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            local configs = require("nvim-treesitter.configs")
+	--git
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "VeryLazy",
+		opts = {
+			current_line_blame = true,
+			current_line_blame_opts = {
+				virt_text_pos = "right_align",
+				delay = 500,
+			},
+		},
+	},
+	--treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
 
-            configs.setup({
-                ensure_installed = { "lua", "python", "java", "javascript", "html" },
-                auto_install = true,
-                sync_install = false,
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end
-    },
-    --auto close brackets
-    { 'cohama/lexima.vim' },
-    --auto indent to current level
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        event = 'VeryLazy',
-        main = "ibl",
-        opts = {}
-    },
-    --cmp
-    {
-        "hrsh7th/nvim-cmp",
-        version = false, -- last release is way too old
-        event = "VeryLazy",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            'L3MON4D3/LuaSnip',
-            'hrsh7th/cmp-cmdline',
-            'hrsh7th/cmp-buffer',
-            'onsails/lspkind.nvim',
-        },
-        opts = function()
-            local luasnip = require 'luasnip'
-            local lspkind = require 'lspkind'
-            local cmp = require 'cmp'
+			configs.setup({
+				ensure_installed = { "lua", "python", "java", "javascript", "html" },
+				auto_install = true,
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	--auto close brackets
+	{ "cohama/lexima.vim" },
+	--auto indent to current level
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = "VeryLazy",
+		main = "ibl",
+		opts = {},
+	},
+	--cmp
+	{
+		"hrsh7th/nvim-cmp",
+		version = false, -- last release is way too old
+		event = "VeryLazy",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"L3MON4D3/LuaSnip",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-buffer",
+			"onsails/lspkind.nvim",
+		},
+		opts = function()
+			local luasnip = require("luasnip")
+			local lspkind = require("lspkind")
+			local cmp = require("cmp")
 
-            cmp.setup {
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
-                    ['<C-d>'] = cmp.mapping.scroll_docs(4),  -- Down
-                    -- C-b (back) C-f (forward) for snippet placeholder navigation.
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<CR>'] = cmp.mapping.confirm {
-                        select = false,
-                    },
-                    ['<Tab>'] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
-                        else
-                            fallback()
-                        end
-                    end, { 'i', 's' }),
-                    ['<S-Tab>'] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { 'i', 's' }),
-                }),
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                sources = {
-                    { name = 'nvim_lsp' },
-                    { name = 'luasnip' },
-                    { name = 'buffer' },
-                },
-                formatting = {
-                    format = lspkind.cmp_format({
-                        mode = 'symbol',       -- show only symbol annotations
-                        maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-                        ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-u>"] = cmp.mapping.scroll_docs(-4), -- Up
+					["<C-d>"] = cmp.mapping.scroll_docs(4), -- Down
+					-- C-b (back) C-f (forward) for snippet placeholder navigation.
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<CR>"] = cmp.mapping.confirm({
+						select = false,
+					}),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+				},
+				formatting = {
+					format = lspkind.cmp_format({
+						mode = "symbol", -- show only symbol annotations
+						maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+						ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 
-                        -- The function below will be called before any actual modifications from lspkind
-                        -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-                        before = function(entry, vim_item)
-                            return vim_item
-                        end
-                    })
-                }
-            }
-            -- `/` cmdline setup.
-            cmp.setup.cmdline('/', {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = {
-                    { name = 'buffer' }
-                }
-            })
-            -- `:` cmdline setup.
-            cmp.setup.cmdline(':', {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                    { name = 'path' }
-                }, {
-                    {
-                        name = 'cmdline',
-                        option = {
-                            ignore_cmds = { 'Man', '!' }
-                        }
-                    }
-                })
-            })
-        end
-    },
-    --lsp diagnostics
-    {
-        "folke/trouble.nvim",
-        event = "VeryLazy",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-    },
-    --better commenting
-    {
-        'numToStr/Comment.nvim',
-        event = 'VeryLazy',
-        opts = {
-            -- add any options here
-        },
-    },
-    --function overview
-    {
-        'stevearc/aerial.nvim',
-        -- Optional dependencies
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-tree/nvim-web-devicons"
-        },
-        event = "VeryLazy",
-        opts = {},
-    },
-    --autoformat
-    {
-        "stevearc/conform.nvim",
-        event = { "BufWritePre" },
-        cmd = { "ConformInfo" },
-        keys = {
-            {
-                -- Customize or remove this keymap to your liking
-                "<leader>cf",
-                function()
-                    require("conform").format({ async = true, lsp_fallback = true })
-                end,
-                mode = "",
-                desc = "Format buffer",
-            },
-        },
-        -- Everything in opts will be passed to setup()
-        opts = {
-            -- Define your formatters
-            formatters_by_ft = {
-                lua = { "stylua" },
-                python = { "isort", "black" },
-                javascript = { { "prettierd", "prettier" } },
-                bash = { "shfmt" },
-                sh = { "shfmt" },
-                zsh = { "shfmt" }
-            },
-            -- Set up format-on-save
-            format_on_save = { timeout_ms = 500, lsp_fallback = true },
-            -- Customize formatters
-            formatters = {
-                shfmt = {
-                    prepend_args = { "-i", "2" },
-                },
-            },
-        },
-        init = function()
-            -- If you want the formatexpr, here is the place to set it
-            vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-        end,
-    }
+						-- The function below will be called before any actual modifications from lspkind
+						-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+						before = function(entry, vim_item)
+							return vim_item
+						end,
+					}),
+				},
+			})
+			-- `/` cmdline setup.
+			cmp.setup.cmdline("/", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+			-- `:` cmdline setup.
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{
+						name = "cmdline",
+						option = {
+							ignore_cmds = { "Man", "!" },
+						},
+					},
+				}),
+			})
+		end,
+	},
+	--lsp diagnostics
+	{
+		"folke/trouble.nvim",
+		event = "VeryLazy",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+	--better commenting
+	{
+		"numToStr/Comment.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+	},
+	--function overview
+	{
+		"stevearc/aerial.nvim",
+		-- Optional dependencies
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+		event = "VeryLazy",
+		opts = {},
+	},
+	--autoformat
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				-- Customize or remove this keymap to your liking
+				"<leader>cf",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
+		-- Everything in opts will be passed to setup()
+		opts = {
+			-- Define your formatters
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "usort", "black" },
+				javascript = { { "prettierd", "prettier" } },
+				bash = { "shfmt" },
+				sh = { "shfmt" },
+				zsh = { "shfmt" },
+			},
+			-- Set up format-on-save
+			format_on_save = { timeout_ms = 500, lsp_fallback = false },
+			-- Customize formatters
+			formatters = {
+				shfmt = {
+					prepend_args = { "-i", "2" },
+				},
+			},
+		},
+		init = function()
+			-- If you want the formatexpr, here is the place to set it
+			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+		end,
+	},
 }
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,214 +1,212 @@
 local signs = {
-    DiagnosticSignError = "󰅚 ",
-    DiagnosticSignWarn = "󰀪 ",
-    DiagnosticSignHint = "󰌶 ",
-    DiagnosticSignInfo = " ",
-    DapBreakpoint = "",
-    DapBreakpointCondition = "",
-    DapBreakpointRejected = "",
-    DapLogPoint = ".>",
-    DapStopped = "󰁕",
+	DiagnosticSignError = "󰅚 ",
+	DiagnosticSignWarn = "󰀪 ",
+	DiagnosticSignHint = "󰌶 ",
+	DiagnosticSignInfo = " ",
+	DapBreakpoint = "",
+	DapBreakpointCondition = "",
+	DapBreakpointRejected = "",
+	DapLogPoint = ".>",
+	DapStopped = "󰁕",
 }
 for type, icon in pairs(signs) do
-    vim.fn.sign_define(type, { text = icon, texthl = type, numhl = type })
+	vim.fn.sign_define(type, { text = icon, texthl = type, numhl = type })
 end
 
 return {
-    --telescope
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.3',
-        dependencies = {
-            {
-                'nvim-lua/plenary.nvim'
-            },
-            {
-                'junegunn/fzf.vim',
-                event = 'VeryLazy'
-            },
-            {
-                'nvim-telescope/telescope-fzf-native.nvim',
-                build =
-                'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-            },
-            {
-                'nvim-telescope/telescope-live-grep-args.nvim',
-                -- This will not install any breaking changes.
-                -- For major updates, this must be adjusted manually.
-                version = '^1.0.0',
-            },
-            {
-                'nvim-tree/nvim-web-devicons'
-            },
-        },
-        config = function()
-            require('telescope').setup {
-                extensions = {
-                    fzf = {
-                        fuzzy = true,
-                        override_generic_sorter = true,
-                        override_file_sorter = true,
-                        case_mode = 'smart_case',
-                    },
-                }
-            }
+	--telescope
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.3",
+		dependencies = {
+			{
+				"nvim-lua/plenary.nvim",
+			},
+			{
+				"junegunn/fzf.vim",
+				event = "VeryLazy",
+			},
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "gmake",
+			},
+			{
+				"nvim-telescope/telescope-live-grep-args.nvim",
+				-- This will not install any breaking changes.
+				-- For major updates, this must be adjusted manually.
+				version = "^1.0.0",
+			},
+			{
+				"nvim-tree/nvim-web-devicons",
+			},
+		},
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = true,
+						override_file_sorter = true,
+						case_mode = "smart_case",
+					},
+				},
+			})
 
-            require('telescope').load_extension('fzf')
-            require('telescope').load_extension('live_grep_args')
-            require("telescope").load_extension('harpoon')
-        end
-    },
-    --whichkey
-    {
-        'folke/which-key.nvim',
-        event = 'VeryLazy',
-        init = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-        end,
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        }
-    },
-    --nvim-tree
-    {
-        'nvim-tree/nvim-tree.lua',
-        event = 'VeryLazy',
-        dependencies = {
-            'nvim-tree/nvim-web-devicons'
-        },
-        opts = {
-            sort_by = 'case_sensitive',
-            view = {
-                width = 40,
-            },
-            renderer = {
-                group_empty = true,
-            },
-            filters = {
-                dotfiles = false,
-            },
-        }
-    },
-    --ufo
-    {
-        'kevinhwang91/nvim-ufo',
-        dependencies = {
-            'kevinhwang91/promise-async'
-        },
-        event = 'VeryLazy',
-        config = function()
-            local ftMap = {
-                vim = 'indent',
-                python = { 'indent' },
-                git = ''
-            }
+			require("telescope").load_extension("fzf")
+			require("telescope").load_extension("live_grep_args")
+			require("telescope").load_extension("harpoon")
+		end,
+	},
+	--whichkey
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+	--nvim-tree
+	{
+		"nvim-tree/nvim-tree.lua",
+		event = "VeryLazy",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {
+			sort_by = "case_sensitive",
+			view = {
+				width = 40,
+			},
+			renderer = {
+				group_empty = true,
+			},
+			filters = {
+				dotfiles = false,
+			},
+		},
+	},
+	--ufo
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = {
+			"kevinhwang91/promise-async",
+		},
+		event = "VeryLazy",
+		config = function()
+			local ftMap = {
+				vim = "indent",
+				python = { "indent" },
+				git = "",
+			}
 
-            local handler = function(virtText, lnum, endLnum, width, truncate)
-                local newVirtText = {}
-                local suffix = (' 󰁂 %d '):format(endLnum - lnum)
-                local sufWidth = vim.fn.strdisplaywidth(suffix)
-                local targetWidth = width - sufWidth
-                local curWidth = 0
-                for _, chunk in ipairs(virtText) do
-                    local chunkText = chunk[1]
-                    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-                    if targetWidth > curWidth + chunkWidth then
-                        table.insert(newVirtText, chunk)
-                    else
-                        chunkText = truncate(chunkText, targetWidth - curWidth)
-                        local hlGroup = chunk[2]
-                        table.insert(newVirtText, { chunkText, hlGroup })
-                        chunkWidth = vim.fn.strdisplaywidth(chunkText)
-                        -- str width returned from truncate() may less than 2nd argument, need padding
-                        if curWidth + chunkWidth < targetWidth then
-                            suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
-                        end
-                        break
-                    end
-                    curWidth = curWidth + chunkWidth
-                end
-                table.insert(newVirtText, { suffix, 'MoreMsg' })
-                return newVirtText
-            end
+			local handler = function(virtText, lnum, endLnum, width, truncate)
+				local newVirtText = {}
+				local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+				local sufWidth = vim.fn.strdisplaywidth(suffix)
+				local targetWidth = width - sufWidth
+				local curWidth = 0
+				for _, chunk in ipairs(virtText) do
+					local chunkText = chunk[1]
+					local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					if targetWidth > curWidth + chunkWidth then
+						table.insert(newVirtText, chunk)
+					else
+						chunkText = truncate(chunkText, targetWidth - curWidth)
+						local hlGroup = chunk[2]
+						table.insert(newVirtText, { chunkText, hlGroup })
+						chunkWidth = vim.fn.strdisplaywidth(chunkText)
+						-- str width returned from truncate() may less than 2nd argument, need padding
+						if curWidth + chunkWidth < targetWidth then
+							suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+						end
+						break
+					end
+					curWidth = curWidth + chunkWidth
+				end
+				table.insert(newVirtText, { suffix, "MoreMsg" })
+				return newVirtText
+			end
 
-            require('ufo').setup({
-                open_fold_hl_timeout = 150,
-                close_fold_kinds = { 'imports', 'comment' },
-                preview = {
-                    win_config = {
-                        border = { '', '─', '', '', '', '─', '', '' },
-                        winhighlight = 'Normal:Folded',
-                        winblend = 0
-                    },
-                    mappings = {
-                        scrollU = '<C-u>',
-                        scrollD = '<C-d>',
-                        jumpTop = '[',
-                        jumpBot = ']'
-                    }
-                },
-                provider_selector = function(filetype)
-                    -- if you prefer treesitter provider rather than lsp,
-                    -- return ftMap[filetype] or {'treesitter', 'indent'}
-                    return ftMap[filetype]
+			require("ufo").setup({
+				open_fold_hl_timeout = 150,
+				close_fold_kinds = { "imports", "comment" },
+				preview = {
+					win_config = {
+						border = { "", "─", "", "", "", "─", "", "" },
+						winhighlight = "Normal:Folded",
+						winblend = 0,
+					},
+					mappings = {
+						scrollU = "<C-u>",
+						scrollD = "<C-d>",
+						jumpTop = "[",
+						jumpBot = "]",
+					},
+				},
+				provider_selector = function(filetype)
+					-- if you prefer treesitter provider rather than lsp,
+					-- return ftMap[filetype] or {'treesitter', 'indent'}
+					return ftMap[filetype]
 
-                    -- refer to ./doc/example.lua for detail
-                end,
-                fold_virt_text_handler = handler
-            })
-        end
-    },
-    --cleaner UI
-    {
-        'folke/noice.nvim',
-        event = 'VeryLazy',
-        dependencies = {
-            'MunifTanjim/nui.nvim',
-            'rcarriga/nvim-notify',
-        },
-        opts = {
-            lsp = {
-                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                override = {
-                    ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-                    ['vim.lsp.util.stylize_markdown'] = true,
-                    ['cmp.entry.get_documentation'] = true,
-                },
-            },
-            popupmenu = {
-                -- cmp-cmdline has more sources and can be extended
-                backend = 'cmp', -- backend to use to show regular cmdline completions
-            },
-            -- you can enable a preset for easier configuration
-            presets = {
-                bottom_search = true,         -- use a classic bottom cmdline for search
-                command_palette = true,       -- position the cmdline and popupmenu together
-                long_message_to_split = true, -- long messages will be sent to a split
-                inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-                lsp_doc_border = false,       -- add a border to hover docs and signature help
-            },
-        }
-    },
-    --lazygit
-    {
-        'kdheepak/lazygit.nvim',
-        event = 'VeryLazy',
-        -- optional for floating window border decoration
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-        },
-    },
-    {
-        'ThePrimeagen/harpoon',
-        event = "VeryLazy",
-        opts = {
-            global_settings = {
-                mark_branch = true
-            }
-        }
-    }
-
+					-- refer to ./doc/example.lua for detail
+				end,
+				fold_virt_text_handler = handler,
+			})
+		end,
+	},
+	--cleaner UI
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+		opts = {
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
+			popupmenu = {
+				-- cmp-cmdline has more sources and can be extended
+				backend = "cmp", -- backend to use to show regular cmdline completions
+			},
+			-- you can enable a preset for easier configuration
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+		},
+	},
+	--lazygit
+	{
+		"kdheepak/lazygit.nvim",
+		event = "VeryLazy",
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{
+		"ThePrimeagen/harpoon",
+		event = "VeryLazy",
+		opts = {
+			global_settings = {
+				mark_branch = true,
+			},
+		},
+	},
 }
