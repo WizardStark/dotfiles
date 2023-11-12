61771b46b7edbd749e60304d2aac5a3e92b133d5
WizardStark <juan.barnard10@gmail.com>
2023-11-12 21:47:33 +0200

    Add friendly snippets for a much larger snippet library

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -48,7 +48,10 @@ return {
 		event = "VeryLazy",
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
-			"L3MON4D3/LuaSnip",
+			{
+				"L3MON4D3/LuaSnip",
+				dependencies = { "rafamadriz/friendly-snippets" },
+			},
 			"hrsh7th/cmp-cmdline",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-calc",
