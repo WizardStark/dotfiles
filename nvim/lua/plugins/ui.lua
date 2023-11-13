c1cb9f16dfe3e332accece6bba9fee8d27d8bc46
WizardStark <juan.barnard10@gmail.com>
2023-11-13 15:26:46 +0200

    Disable animations in notify.nvim for better performance

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -239,7 +239,12 @@ return {
 		event = "VeryLazy",
 		dependencies = {
 			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
+			{
+				"rcarriga/nvim-notify",
+				opts = {
+					stages = "static",
+				},
+			},
 		},
 		opts = {
 			lsp = {
