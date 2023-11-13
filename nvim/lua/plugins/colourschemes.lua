2ba621294375c80cda764f7094d35278a9f33397
WizardStark <juan.barnard10@gmail.com>
2023-11-13 15:01:28 +0200

    Add alacritty config and clean up unused nvim theme

--- nvim/lua/plugins/colourschemes.lua
+++ nvim/lua/plugins/colourschemes.lua
@@ -1,20 +1,11 @@
 return {
-    {
-        "bluz71/vim-nightfly-colors",
-        name = "nightfly",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            -- vim.cmd [[colorscheme nightfly]]
-        end
-    },
-    {
-        "bluz71/vim-moonfly-colors",
-        name = "moonfly",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.cmd [[colorscheme moonfly]]
-        end
-    },
+	{
+		"bluz71/vim-moonfly-colors",
+		name = "moonfly",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme moonfly]])
+		end,
+	},
 }
