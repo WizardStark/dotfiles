62ae18cb50fdeb03b1827860960961f1264c4fda
WizardStark <juan.barnard10@gmail.com>
2023-11-29 19:48:52 +0200

    Add colemak

--- keyboard/wizardstark/keymap.c
+++ keyboard/wizardstark/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
          KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,           KC_ENT,          KC_PGDN,
         KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,                   KC_RSFT,  KC_UP,   KC_END,
-        KC_LCTL, KC_LGUI, KC_LALT,                             KC_SPC,                            KC_RALT,   MO(3), KC_RCTL, KC_LEFT,KC_DOWN,  KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                             KC_SPC,                            KC_RALT,   MO(4), KC_RCTL, KC_LEFT,KC_DOWN,  KC_RGHT
     ),
     // Windows function layer
     [1] = LAYOUT(
@@ -50,16 +50,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_1,    KC_2,    KC_3,            KC_NO,   KC_NO,   KC_NO,
           KC_NO,   KC_NO,   KC_NO,                             KC_ENT,                               KC_0,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
     ),
-    // Adjust layer
     [3] = LAYOUT(
+        KC_CAPS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_PSCR,          KC_MPLY,
+         KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSPC,           KC_DEL,
+         KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
+         KC_ESC,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT,           KC_ENT,          KC_PGDN,
+        KC_LSFT,    KC_X,    KC_C,    KC_D,    KC_V,    KC_Z,    KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,                   KC_RSFT,  KC_UP,   KC_END,
+        KC_LCTL, KC_LGUI, KC_LALT,                             KC_SPC,                            KC_RALT,   MO(4), KC_RCTL, KC_LEFT,KC_DOWN,  KC_RGHT
+    ),
+    // Adjust layer
+    [4] = LAYOUT(
     //   KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_PSCR,          KC_MUTE,
           TG(2), KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,          _______,
-        _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,          _______,
+        _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            TO(0),
+        _______, _______, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,            TO(3),
         _______, _______, RGB_VAD, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
         _______,          _______, RGB_HUI, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,          _______, RGB_MOD, _______,
         _______, _______, _______,                            _______,                            _______, _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
-    )
+    ),
+
 };
 // clang-format on
 
@@ -68,11 +77,12 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
     [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-    [3] = { ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
+    [3] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [4] = { ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
 };
 #endif
 
-uint8_t l3_keys_to_change[] = {6,7,12,14,15,16,18,23,28,34,38,39,44,50,56,61,66,72,75,79,94,95,97};
+uint8_t l4_keys_to_change[] = {6,7,12,14,15,16,18,23,28,34,38,39,44,50,56,61,66,72,75,79,94,95,97};
 
 uint8_t l2_keys_to_change[] = {3,33,46,47,48,49,52,53,54,58,59,60};
 
@@ -84,6 +94,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
             for(int i=0;i<NELEMS(LED_OUTER);i++){
                 rgb_matrix_set_color(LED_OUTER[i], 135, 206, 235);
             }
+            rgb_matrix_set_color(72, 255, 0, 255);
             break;
         case 1:
             rgb_matrix_set_color_all(0,0,0);
@@ -99,8 +110,16 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
             break;
         case 3:
             rgb_matrix_set_color_all(0,0,0);
-            for(int i=0;i<NELEMS(l3_keys_to_change);i++){
-                rgb_matrix_set_color(l3_keys_to_change[i], 255, 0, 255);
+            for(int i=0;i<NELEMS(LED_OUTER);i++){
+                rgb_matrix_set_color(LED_OUTER[i], 135, 206, 235);
+            }
+            rgb_matrix_set_color(75, 255, 0, 255);
+            break;
+ 
+        case 4:
+            rgb_matrix_set_color_all(0,0,0);
+            for(int i=0;i<NELEMS(l4_keys_to_change);i++){
+                rgb_matrix_set_color(l4_keys_to_change[i], 255, 0, 255);
             }
             break;
         default:
