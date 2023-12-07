a039785c288ade4dcadee5117832d81bded7ad45
WizardStark <juan.barnard10@gmail.com>
2023-12-07 08:36:32 +0200

    Remove lsp progress messages from noice

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -246,6 +246,10 @@ return {
 		opts = {
 			lsp = {
 				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				progress = {
+					enabled = false,
+				},
+
 				override = {
 					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 					["vim.lsp.util.stylize_markdown"] = true,
