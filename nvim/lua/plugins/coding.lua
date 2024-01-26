482535d8612898fda2087cf7c72ce5a2a6a74a48
WizardStark <juan.barnard10@gmail.com>
2024-01-26 12:22:10 +0200

    Fix snippet configuration

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -50,18 +50,15 @@ return {
 	{
 		"hrsh7th/nvim-cmp",
 		version = false, -- last release is way too old
-		event = "VeryLazy",
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
-			{
-				"L3MON4D3/LuaSnip",
-				dependencies = { "rafamadriz/friendly-snippets" },
-			},
+			"L3MON4D3/LuaSnip",
 			"hrsh7th/cmp-cmdline",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-calc",
 			"onsails/lspkind.nvim",
 			"hrsh7th/cmp-nvim-lsp-signature-help",
+			"saadparwaiz1/cmp_luasnip",
 		},
 		opts = function()
 			local luasnip = require("luasnip")
@@ -72,46 +69,50 @@ return {
 				scrollbar = true,
 			})
 
+			local function tab(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif luasnip.expand_or_jumpable() then
+					luasnip.expand_or_jump()
+				else
+					fallback()
+				end
+			end
+
+			local function shift_tab(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif luasnip.jumpable(-1) then
+					luasnip.jump(-1)
+				else
+					fallback()
+				end
+			end
+
 			cmp.setup({
 				snippet = {
 					expand = function(args)
 						luasnip.lsp_expand(args.body)
 					end,
 				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-u>"] = cmp.mapping.scroll_docs(-4), -- Up
-					["<C-d>"] = cmp.mapping.scroll_docs(4), -- Down
-					-- C-b (back) C-f (forward) for snippet placeholder navigation.
+				mapping = {
+					["<Tab>"] = cmp.mapping(tab, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(shift_tab, { "i", "s" }),
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.close(),
 					["<CR>"] = cmp.mapping.confirm({
 						select = false,
 					}),
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.expand_or_jumpable() then
-							luasnip.expand_or_jump()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-				}),
+				},
 				window = {
 					documentation = window_scroll_bordered,
 					completion = window_scroll_bordered,
 				},
 				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
+					{ name = "nvim_lsp", max_item_count = 5 },
+					{ name = "luasnip", max_item_count = 8 },
 					{ name = "codeium" },
 					{ name = "calc" },
 					{ name = "nvim_lsp_signature_help" },
@@ -155,6 +156,33 @@ return {
 				}),
 			})
 		end,
+	},
+
+	--snippets
+	{
+		"mireq/luasnip-snippets",
+		dependencies = { "L3MON4D3/LuaSnip" },
+		init = function()
+			require("luasnip_snippets.common.snip_utils").setup()
+		end,
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			"nvim-treesitter/nvim-treesitter",
+			init = function()
+				local ls = require("luasnip")
+				ls.setup({
+					load_ft_func = require("luasnip_snippets.common.snip_utils").load_ft_func,
+					ft_func = require("luasnip_snippets.common.snip_utils").ft_func,
+					enable_autosnippets = true,
+					history = true,
+					updateevents = "TextChanged,TextChangedI",
+				})
+				require("luasnip/loaders/from_vscode").lazy_load()
+			end,
+		},
 	},
 	--better commenting
 	{
