a5594ca8dc74613e7bd6e832d77d159473ec314d
WizardStark <juan.barnard10@gmail.com>
2024-02-05 13:20:57 +0200

    Disable treesitter if filesize > 500kb

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -30,7 +30,16 @@ return {
 				ensure_installed = { "lua", "python", "java", "javascript", "html" },
 				auto_install = true,
 				sync_install = false,
-				highlight = { enable = true },
+				highlight = {
+					enable = true,
+					disable = function(lang, buf)
+						local max_filesize = 500 * 1024 -- 500 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							return true
+						end
+					end,
+				},
 				indent = { enable = true },
 			})
 		end,
