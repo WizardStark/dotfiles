66b4e3ec2b51080185e67639ca98f34728483659
WizardStark <juan.barnard10@gmail.com>
2024-02-14 13:33:57 +0200

    Remove unnecessary overrides of noice options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -30,17 +30,4 @@ opt.undofile = true
 opt.undodir = vim.fn.expand("~/.undo")
 opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
-vim.notify = require("notify")
-
--- table from lsp severity to vim severity.
-local severity = {
-	"error",
-	"warn",
-	"info",
-	"info", -- map both hint and info to info?
-}
-vim.lsp.handlers["window/showMessage"] = function(err, method, params, client_id)
-	vim.notify(method.message, severity[params.type])
-end
-
 return {}
