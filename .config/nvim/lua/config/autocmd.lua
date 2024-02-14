889e704bfa8bf424ba4dc478659b1de5c1f3fdab
WizardStark <juan.barnard10@gmail.com>
2024-02-14 11:57:55 +0200

    Revert previous commit

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -1,33 +1,3 @@
-local function is_restorable(buffer)
-	if #vim.api.nvim_buf_get_option(buffer, "bufhidden") ~= 0 then
-		return false
-	end
-
-	local buftype = vim.api.nvim_buf_get_option(buffer, "buftype")
-	if #buftype == 0 then
-		-- Normal buffer, check if it listed.
-		if not vim.api.nvim_buf_get_option(buffer, "buflisted") then
-			return false
-		end
-		-- Check if it has a filename.
-		if #vim.api.nvim_buf_get_name(buffer) == 0 then
-			return false
-		end
-	elseif buftype ~= "terminal" and buftype ~= "help" then
-		-- Buffers other then normal, terminal and help are impossible to restore.
-		return false
-	end
-end
-
-local function is_restorable_buffer_present()
-	for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
-		if vim.api.nvim_buf_is_valid(buffer) and is_restorable(buffer) then
-			return true
-		end
-	end
-	return false
-end
-
 require("legendary").autocmds({
 	{
 		"FileType",
@@ -68,10 +38,8 @@ require("legendary").autocmds({
 		{
 			"VimLeavePre",
 			function()
-				if is_restorable_buffer_present() then
-					if string.find(vim.fn.expand("%:p"), vim.fn.getcwd()) then
-						require("session_manager").save_current_session()
-					end
+				if string.find(vim.fn.expand("%:p"), vim.fn.getcwd()) then
+					require("session_manager").save_current_session()
 				end
 			end,
 		},
