5701fd38ad723039d87ff8e3353024ffc79a9a5a
WizardStark <juan.barnard10@gmail.com>
2024-02-15 10:44:47 +0200

    Add handling for git commit buffers in flatten.nvim

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -99,10 +99,51 @@ return {
 		"willothy/flatten.nvim",
 		lazy = false,
 		priority = 1001,
-		opts = {
-			window = {
-				open = "alternate",
-			},
-		},
+		opts = function()
+			local saved_terminal
+			return {
+				window = {
+					open = "alternate",
+				},
+				callbacks = {
+					should_block = function(argv)
+						return vim.tbl_contains(argv, "-b")
+					end,
+					pre_open = function()
+						local term = require("toggleterm.terminal")
+						local termid = term.get_focused_id()
+						saved_terminal = term.get(termid)
+					end,
+					post_open = function(bufnr, winnr, ft, is_blocking)
+						if is_blocking and saved_terminal then
+							saved_terminal:close()
+						else
+							vim.api.nvim_set_current_win(winnr)
+						end
+
+						-- If the file is a git commit, create one-shot autocmd to delete its buffer on write
+						-- If you just want the toggleable terminal integration, ignore this bit
+						if ft == "gitcommit" or ft == "gitrebase" then
+							vim.api.nvim_create_autocmd("BufWritePost", {
+								buffer = bufnr,
+								once = true,
+								callback = vim.schedule_wrap(function()
+									vim.api.nvim_buf_delete(bufnr, {})
+								end),
+							})
+						end
+					end,
+					block_end = function()
+						-- After blocking ends (for a git commit, etc), reopen the terminal
+						vim.schedule(function()
+							if saved_terminal then
+								saved_terminal:open()
+								saved_terminal = nil
+							end
+						end)
+					end,
+				},
+			}
+		end,
 	},
 }
