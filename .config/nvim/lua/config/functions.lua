7b6f9ec7b63d07dee500c4751537a9056e99ec1d
WizardStark <juan.barnard10@gmail.com>
2024-02-21 17:30:10 +0200

    Add local config related funcs

--- .config/nvim/lua/config/functions.lua
+++ .config/nvim/lua/config/functions.lua
@@ -7,9 +7,23 @@ return {
 					vim.fn.system(
 						"mkdir -p ~/.config/lcl && touch ~/.config/lcl/init.lua && echo M={} return M >> ~/.config/lcl/init.lua"
 					)
+					vim.notify("Please close and reopen vim")
+				else
+					vim.notify("Local config already exists")
 				end
 			end,
 			description = "Create local config file if it does not exist",
+		},
+		{
+			function()
+				local ok, _ = pcall(dofile, vim.fn.expand("$HOME/.config/lcl/init.lua"))
+				if not ok then
+					vim.cmd.e("~/.config/lcl/init.lua")
+				else
+					vim.notify("Local config does not exist")
+				end
+			end,
+			description = "Edit local config",
 		},
 	}),
 }
