3434924d389663f9a304e39f4d7ee0951a67c3b7
Reghardt <reghardtjooste@live.com>
2024-02-29 09:51:59 +0200

    Remove prettier args in favour of .prettierrc files

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -267,9 +267,6 @@ return {
 			},
 			format_on_save = false,
 			formatters = {
-				prettier = {
-					prepend_args = { "--tab-width", "4" },
-				},
 				shfmt = {
 					prepend_args = { "-i", "2" },
 				},
