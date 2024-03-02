1d24e94c3b7a927242ad6b8cc739c3d809e0e784
WizardStark <juan.barnard10@gmail.com>
2024-03-02 17:10:20 +0200

    Move font size and cursor config to lcl.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -33,9 +33,6 @@ opt.swapfile = false
 opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
 if vim.g.neovide then
-    vim.o.guifont = "Hack Nerd Font Mono:h12"
-    vim.g.neovide_cursor_trail_size = 0.2
-    vim.g.neovide_cursor_vfx_mode = "pixiedust"
 end
 
 return {}
