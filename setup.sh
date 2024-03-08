3162bce68da70d17a52218764b8b948bdb5856a1
WizardStark <juan.barnard10@gmail.com>
2024-03-08 16:51:33 +0200

    Remove lazygit from setup.sh

--- setup.sh
+++ setup.sh
@@ -40,12 +40,6 @@ rm -rf neovim
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --key-bindings --completion --update-rc
 
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
-rm lazygit*
-
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
 cd "$directory"
