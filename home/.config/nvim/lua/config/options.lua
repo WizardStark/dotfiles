28a77cd63a1cb379603e9e5f021bc0f1063df4cc
WizardStark <juan.barnard10@gmail.com>
2024-05-06 10:09:02 +0200

    Add mechanism to override vim options via lcl/options.lua

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -3,51 +3,59 @@ vim.g.loaded_netrwPlugin = 1
 
 local opt = vim.opt
 
-opt.foldcolumn = "0"
-opt.foldlevel = 99
-opt.foldlevelstart = 99
-opt.foldenable = true
-opt.termguicolors = true
-opt.showmatch = true
-opt.incsearch = true
-opt.hlsearch = false
-opt.tabstop = 4
-opt.expandtab = true
-opt.shiftwidth = 4
-opt.autoindent = true
-opt.number = true
-opt.relativenumber = true
-opt.wildmode = "longest,list"
-opt.cursorline = true
-opt.ttyfast = true
-opt.laststatus = 2
-opt.pumheight = 10
-opt.signcolumn = "yes"
-opt.updatetime = 100
-opt.fillchars = {
-	vert = "│",
-	eob = " ",
+local opts = {
+	foldcolumn = "0",
+	foldlevel = 99,
+	foldlevelstart = 99,
+	foldenable = true,
+	termguicolors = true,
+	showmatch = true,
+	incsearch = true,
+	hlsearch = false,
+	tabstop = 4,
+	expandtab = true,
+	shiftwidth = 4,
+	autoindent = true,
+	number = true,
+	relativenumber = true,
+	wildmode = "longest,list",
+	cursorline = true,
+	ttyfast = true,
+	laststatus = 2,
+	pumheight = 10,
+	signcolumn = "yes",
+	updatetime = 100,
+	fillchars = {
+		vert = "│",
+		eob = " ",
+	},
+	scrolloff = 8,
+	undofile = true,
+	showtabline = 1,
+	undodir = { vim.fn.expand("~/.undo") },
+	swapfile = false,
+	sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions",
+	grepprg = "rg --vimgrep",
+	grepformat = "%f:%l:%c:%m",
+	-- from TJ's config
+	formatoptions = opt.formatoptions
+		- "a" -- Auto formatting is BAD.
+		- "t" -- Don't auto format my code. I got linters for that.
+		+ "c" -- In general, I like it when comments respect textwidth
+		+ "q" -- Allow formatting comments w/ gq
+		- "o" -- O and o, don't continue comments
+		+ "r" -- But do continue when pressing enter.
+		+ "n" -- Indent past the formatlistpat, not underneath it.
+		+ "j" -- Auto-remove comments if possible.
+		- "2", -- I'm not in gradeschool anymore
 }
-opt.scrolloff = 8
-opt.undofile = true
-opt.showtabline = 1
-opt.undodir = { vim.fn.expand("~/.undo") }
-opt.swapfile = false
-opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-opt.grepprg = "rg --vimgrep"
-opt.grepformat = "%f:%l:%c:%m"
 
--- from TJ's config
-opt.formatoptions = opt.formatoptions
-	- "a" -- Auto formatting is BAD.
-	- "t" -- Don't auto format my code. I got linters for that.
-	+ "c" -- In general, I like it when comments respect textwidth
-	+ "q" -- Allow formatting comments w/ gq
-	- "o" -- O and o, don't continue comments
-	+ "r" -- But do continue when pressing enter.
-	+ "n" -- Indent past the formatlistpat, not underneath it.
-	+ "j" -- Auto-remove comments if possible.
-	- "2" -- I'm not in gradeschool anymore
+require("lcl.options")
+opts = vim.tbl_deep_extend("force", opts, vim.g.overridden_opts or {})
+
+for key, value in pairs(opts) do
+	vim.opt[key] = value
+end
 
 vim.g.colorscheme = "catppuccin-mocha"
 
