92101f2f8a8ea15d55011a22a9d12145a6c83d59
WizardStark <juan.barnard10@gmail.com>
2024-05-13 10:52:21 +0200

    Revert changes to java config

--- home/.config/nvim/lua/plugins/java.lua
+++ home/.config/nvim/lua/plugins/java.lua
@@ -10,7 +10,6 @@ return {
 		},
 		config = function(_, opts)
 			local resolve_opts = function()
-				local jdtls = require("jdtls")
 				local Path = require("plenary.path")
 				local root_dir = require("jdtls.setup").find_root(opts.root_markers)
 				local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
@@ -19,9 +18,8 @@ return {
 					Path:new(workspace_dir):mkdir({ parents = true })
 				end
 				local jdtls_path = require("mason-registry").get_package("jdtls"):get_install_path()
-				local java_debug_path = vim.fn.stdpath("data")
-					.. "mason/share/java_debug_adapter/com.microsoft.java.debug.plugin.jar"
-				local java_test_path = vim.fn.stdpath("data") .. "mason/share/java-test"
+				local java_debug_path = require("mason-registry").get_package("java-debug-adapter"):get_install_path()
+				local java_test_path = require("mason-registry").get_package("java-test"):get_install_path()
 				local os
 				if vim.fn.has("macunix") then
 					os = "mac"
@@ -30,9 +28,12 @@ return {
 				end
 
 				local bundles = {
-					vim.fn.glob(java_debug_path, true),
+					vim.fn.glob(java_debug_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar", true),
 				}
-				vim.list_extend(bundles, vim.split(vim.fn.glob(java_test_path .. "/*.jar", true), "\n"))
+				vim.list_extend(
+					bundles,
+					vim.split(vim.fn.glob(java_test_path .. "/extension/server/*.jar", true), "\n")
+				)
 
 				return {
 					cmd = {
@@ -43,7 +44,7 @@ return {
 						"-Dlog.protocol=true",
 						"-Dlog.level=ALL",
 						"-javaagent:" .. jdtls_path .. "/lombok.jar",
-						"-Xmx4g",
+						"-Xmx1g",
 						"--add-modules=ALL-SYSTEM",
 						"--add-opens",
 						"java.base/java.util=ALL-UNNAMED",
@@ -60,91 +61,6 @@ return {
 					init_options = {
 						bundles = bundles,
 					},
-					settings = {
-						java = {
-							-- home = "/usr/lib/jvm/java-17-openjdk-amd64",
-							-- configuration = {
-							-- 	updateBuildConfiguration = "interactive",
-							-- 	runtimes = {
-							-- 		{
-							-- 			name = "JavaSE-11",
-							-- 			path = "/usr/lib/jvm/java-11-openjdk-amd64",
-							-- 		},
-							-- 		{
-							-- 			name = "JavaSE-17",
-							-- 			path = "/usr/lib/jvm/java-17-openjdk-amd64",
-							-- 		},
-							-- 		{
-							-- 			name = "JavaSE-19",
-							-- 			path = "/usr/lib/jvm/java-19-openjdk-amd64",
-							-- 		},
-							-- 	},
-							-- },
-							eclipse = {
-								downloadSources = true,
-							},
-							maven = {
-								downloadSources = true,
-							},
-							implementationsCodeLens = {
-								enabled = true,
-							},
-							referencesCodeLens = {
-								enabled = true,
-							},
-							references = {
-								includeDecompiledSources = true,
-							},
-							signatureHelp = { enabled = true },
-							format = {
-								enabled = true,
-								-- Formatting works by default, but you can refer to a specific file/URL if you choose
-								-- settings = {
-								--   url = "https://github.com/google/styleguide/blob/gh-pages/intellij-java-google-style.xml",
-								--   profile = "GoogleStyle",
-								-- },
-							},
-						},
-						completion = {
-							favoriteStaticMembers = {
-								"org.hamcrest.MatcherAssert.assertThat",
-								"org.hamcrest.Matchers.*",
-								"org.hamcrest.CoreMatchers.*",
-								"org.junit.jupiter.api.Assertions.*",
-								"java.util.Objects.requireNonNull",
-								"java.util.Objects.requireNonNullElse",
-								"org.mockito.Mockito.*",
-							},
-							importOrder = {
-								"java",
-								"javax",
-								"com",
-								"org",
-							},
-						},
-						extendedClientCapabilities = jdtls.extendedClientCapabilities,
-						sources = {
-							organizeImports = {
-								starThreshold = 9999,
-								staticStarThreshold = 9999,
-							},
-						},
-						codeGeneration = {
-							toString = {
-								template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
-							},
-							useBlocks = true,
-						},
-					},
-					-- Needed for auto-completion with method signatures and placeholders
-					capabilities = require("cmp_nvim_lsp").default_capabilities(),
-					flags = {
-						allow_incremental_sync = true,
-					},
-					-- on_attach = function(client, bufnr)
-					-- 	jdtls.setup_dap({ hotcodereplace = "auto" })
-					-- 	require("jdtls.dap").setup_dap_main_class_configs()
-					-- end,
 				}
 			end
 			vim.api.nvim_create_autocmd("Filetype", {
