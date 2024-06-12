f04e7dc7e72fa8aec517cf2e5ee4188cbc9f4eea
WizardStark <juan.barnard10@gmail.com>
2024-06-12 19:44:15 +0200

    Change flatten config

--- home/.config/nvim/lua/plugins/util.lua
+++ home/.config/nvim/lua/plugins/util.lua
@@ -104,41 +104,42 @@ return {
 		"willothy/flatten.nvim",
 		lazy = false,
 		priority = 1001,
-		opts = function()
-			local saved_terminal
-
-			return {
-				window = {
-					open = "alternate",
-				},
-				nest_if_no_args = true,
-				callbacks = {
-					should_block = function(argv)
-						return vim.tbl_contains(argv, "-b")
-					end,
-					pre_open = function()
-						local term = require("toggleterm.terminal")
-						local termid = term.get_focused_id()
-						saved_terminal = term.get(termid)
-					end,
-					post_open = function(bufnr, winnr, ft, is_blocking)
-						if is_blocking and saved_terminal then
-							saved_terminal:close()
-						else
-							vim.api.nvim_set_current_win(winnr)
-						end
-					end,
-					block_end = function()
-						vim.schedule(function()
-							if saved_terminal then
-								saved_terminal:open()
-								saved_terminal = nil
-							end
-						end)
-					end,
-				},
-			}
-		end,
+		opts = {},
+		-- opts = function()
+		-- 	local saved_terminal
+		--
+		-- 	return {
+		-- 		window = {
+		-- 			open = "smart",
+		-- 		},
+		-- 		nest_if_no_args = true,
+		-- 		callbacks = {
+		-- 			should_block = function(argv)
+		-- 				return vim.tbl_contains(argv, "-b")
+		-- 			end,
+		-- 			pre_open = function()
+		-- 				local term = require("toggleterm.terminal")
+		-- 				local termid = term.get_focused_id()
+		-- 				saved_terminal = term.get(termid)
+		-- 			end,
+		-- 			post_open = function(bufnr, winnr, ft, is_blocking)
+		-- 				if is_blocking and saved_terminal then
+		-- 					saved_terminal:close()
+		-- 				else
+		-- 					vim.api.nvim_set_current_win(winnr)
+		-- 				end
+		-- 			end,
+		-- 			block_end = function()
+		-- 				vim.schedule(function()
+		-- 					if saved_terminal then
+		-- 						saved_terminal:open()
+		-- 						saved_terminal = nil
+		-- 					end
+		-- 				end)
+		-- 			end,
+		-- 		},
+		-- 	}
+		-- end,
 	},
 	--paren-hint
 	{
