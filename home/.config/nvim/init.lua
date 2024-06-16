81d94c5e096bf2b0ab827a0cbee8b6995fdbe633
WizardStark <juan.barnard10@gmail.com>
2024-06-16 12:46:09 +0200

    Remove wsl clipboard snippet

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -47,22 +47,6 @@ vim.g.backdrop_buf = nil
 vim.g.backdrop_win = nil
 vim.g.colorscheme = "catppuccin-mocha"
 
--- setup clipboard for wsl
-if vim.fn.has("wsl") == 1 then
-	vim.g.clipboard = {
-		name = "WslClipboard",
-		copy = {
-			["+"] = "clip.exe",
-			["*"] = "clip.exe",
-		},
-		paste = {
-			["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-			["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-		},
-		cache_enabled = 0,
-	}
-end
-
 require("lazy").setup({
 	{ import = "plugins" },
 	{
@@ -147,4 +131,5 @@ require("lazy").setup({
 			},
 		},
 	},
+	-- debug = true,
 })
