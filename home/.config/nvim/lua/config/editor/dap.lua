610ea89441d343f97ffbf01221ca74a4f0af38f8
WizardStark <juan.barnard10@gmail.com>
2024-06-25 13:10:59 +0200

    Fix osv config

--- home/.config/nvim/lua/config/editor/dap.lua
+++ home/.config/nvim/lua/config/editor/dap.lua
@@ -104,3 +104,15 @@ table.insert(require("dap").configurations.python, {
 	program = "${file}",
 	justMyCode = false,
 })
+
+dap.configurations.lua = {
+	{
+		type = "nlua",
+		request = "attach",
+		name = "Attach to running Neovim instance",
+	},
+}
+
+dap.adapters.nlua = function(callback, config)
+	callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
+end
--- home/.config/nvim/lua/plugins/editor.lua
+++ home/.config/nvim/lua/plugins/editor.lua
@@ -111,19 +111,6 @@ return {
 			"mfussenegger/nvim-dap",
 		},
 		lazy = true,
-		config = function()
-			local dap = require("dap")
-			dap.configurations.lua = {
-				{
-					type = "nlua",
-					request = "attach",
-					name = "Attach to running Neovim instance",
-				},
-			}
-			dap.adapters.nlua = function(callback, config)
-				callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
-			end
-		end,
 	},
 	-- LSP
 	{
