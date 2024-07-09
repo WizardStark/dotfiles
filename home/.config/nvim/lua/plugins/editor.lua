6ff42d169733f22b12583ceacdbc8975c50e9778
WizardStark <juan.barnard10@gmail.com>
2024-07-09 12:23:14 +0200

    Switch to mini.pairs for vastly increased performance

--- home/.config/nvim/lua/plugins/editor.lua
+++ home/.config/nvim/lua/plugins/editor.lua
@@ -23,12 +23,23 @@ return {
 		end,
 	},
 	{
-		"altermo/ultimate-autopair.nvim",
-		event = { "InsertEnter", "CmdlineEnter" },
-		branch = "v0.6",
+		"echasnovski/mini.pairs",
+		event = { "InsertEnter" },
+		config = true,
 		opts = {
-			bs = {
-				enable = false,
+			mappings = {
+				-- Prevents the action if the cursor is just before any character or next to a "\".
+				["("] = { action = "open", pair = "()", neigh_pattern = "[^\\][%s%)%]%}]" },
+				["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\][%s%)%]%}]" },
+				["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\][%s%)%]%}]" },
+				-- This is default (prevents the action if the cursor is just next to a "\").
+				[")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+				["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+				["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+				-- Prevents the action if the cursor is just before or next to any character.
+				['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^%w][^%w]", register = { cr = false } },
+				["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%w][^%w]", register = { cr = false } },
+				["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^%w][^%w]", register = { cr = false } },
 			},
 		},
 	},
