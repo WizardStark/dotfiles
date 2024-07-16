0d9ae666c3741e47d97f1a830ef60693bf586ad6
WizardStark <juan.barnard10@gmail.com>
2024-07-16 09:10:02 +0200

    Allow format on save only in certain filetypes

--- home/.config/nvim/lua/config/editor/format.lua
+++ home/.config/nvim/lua/config/editor/format.lua
@@ -1,4 +1,16 @@
 vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+local allowed_filetypes = {
+	"lua",
+	"go",
+	"sh",
+	"zsh",
+}
+
+local function is_allowed_ft(bufnr)
+	return require("user.utils").contains(vim.bo[bufnr].ft, allowed_filetypes)
+end
+
 require("conform").setup({
 	-- Define your formatters
 	formatters_by_ft = {
@@ -21,7 +33,17 @@ require("conform").setup({
 		yml = { "yamlfmt" },
 		yaml = { "yamlfmt" },
 	},
-	format_on_save = true,
+
+	format_on_save = function(bufnr)
+		return {
+			{
+				lsp_format = "fallback",
+				dry_run = is_allowed_ft(bufnr),
+				timeout_ms = 500,
+			},
+		}
+	end,
+
 	formatters = {
 		shfmt = {
 			prepend_args = { "-i", "2" },
