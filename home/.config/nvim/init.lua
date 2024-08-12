787ee648e49a9bbd9868611cac12071de715ba1f
WizardStark <juan.barnard10@gmail.com>
2024-08-12 08:32:22 +0200

    Add localleader

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -2,6 +2,7 @@ vim.loader.enable()
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = " "
+vim.g.maplocalleader = ','
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
