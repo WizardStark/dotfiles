31901b370807d61575fdd58722b14bc6730ef1a1
WizardStark <juan.barnard10@gmail.com>
2024-09-12 11:42:39 +0200

    Bump nvim version

--- setup.sh
+++ setup.sh
@@ -32,7 +32,7 @@ mkdir -p ~/.config
 
 if [[ $(command -v nvim) == "" ]]; then
     (
-        git clone --depth 1 -b v0.10.0 https://github.com/neovim/neovim
+        git clone --depth 1 -b v0.10.1 https://github.com/neovim/neovim
         cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
         sudo make install
         cd ../
