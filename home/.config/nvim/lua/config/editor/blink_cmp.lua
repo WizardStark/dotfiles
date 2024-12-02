7d4de4e4685269ddb32b2c10834820185af7fea7
WizardStark <juan.barnard10@gmail.com>
2024-12-02 12:28:10 +0200

    Update blink.cmp config to add curly braces in latex files

--- home/.config/nvim/lua/config/editor/blink_cmp.lua
+++ home/.config/nvim/lua/config/editor/blink_cmp.lua
@@ -7,7 +7,14 @@ require("blink.cmp").setup({
 		use_nvim_cmp_as_default = true,
 	},
 	nerd_font_variant = "mono",
-	accept = { auto_brackets = { enabled = true } },
+	accept = {
+		auto_brackets = {
+			enabled = true,
+			override_brackets_for_filetypes = {
+				tex = { "{", "}" },
+			},
+		},
+	},
 	trigger = {
 		keyword_range = "full",
 		signature_help = { enabled = true },
@@ -31,12 +38,14 @@ require("blink.cmp").setup({
 			"fallback",
 		},
 	},
-	completion = {
-		enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
-	},
-	providers = {
-		lsp = { fallback_for = { "lazydev" } },
-		lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+	sources = {
+		completion = {
+			enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
+		},
+		providers = {
+			lsp = { fallback_for = { "lazydev" } },
+			lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+		},
 	},
 	windows = {
 		autocomplete = {
