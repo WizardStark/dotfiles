50cfdf08ec25b708c35e0088bf578f8e49661cd1
WizardStark <juan.barnard10@gmail.com>
2024-12-03 12:21:58 +0200

    Update blink.cmp config to v0.7.1

--- home/.config/nvim/lua/config/editor/blink_cmp.lua
+++ home/.config/nvim/lua/config/editor/blink_cmp.lua
@@ -1,29 +1,9 @@
----@param ctx blink.cmp.CompletionRenderContext
----@return blink.cmp.Component
-local function render_item(ctx) end
-
 require("blink.cmp").setup({
-	highlight = {
-		use_nvim_cmp_as_default = true,
-	},
-	nerd_font_variant = "mono",
-	accept = {
-		auto_brackets = {
-			enabled = true,
-			override_brackets_for_filetypes = {
-				tex = { "{", "}" },
-			},
-		},
-	},
-	trigger = {
-		keyword_range = "full",
-		signature_help = { enabled = true },
-	},
 	keymap = {
 		preset = "enter",
 		["<Tab>"] = {
 			function(cmp)
-				if cmp.is_in_snippet() then
+				if cmp.snippet_active() then
 					return cmp.accept()
 				else
 					return require("blink.cmp").select_next()
@@ -38,6 +18,12 @@ require("blink.cmp").setup({
 			"fallback",
 		},
 	},
+	signature = {
+		enabled = true,
+		window = {
+			border = "rounded",
+		},
+	},
 	sources = {
 		completion = {
 			enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
@@ -47,15 +33,30 @@ require("blink.cmp").setup({
 			lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
 		},
 	},
-	windows = {
-		autocomplete = {
+	appearance = {
+		kind_icons = {
+			Snippet = "",
+		},
+	},
+	completion = {
+		keyword = {
+			range = "full",
+		},
+		list = {
+			selection = "auto_insert",
+		},
+		accept = {
+			auto_brackets = {
+				enabled = true,
+				override_brackets_for_filetypes = {
+					tex = { "{", "}" },
+				},
+			},
+		},
+		menu = {
 			min_width = 20,
-			max_width = 60,
-			max_height = 10,
 			border = "rounded",
 			winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
-			scrolloff = 2,
-			direction_priority = { "s", "n" },
 			draw = {
 				columns = { { "kind_icon" }, { "label", "label_description", gap = 1 }, { "source" } },
 				components = {
@@ -70,37 +71,19 @@ require("blink.cmp").setup({
 
 							return map[ctx.item.source_id]
 						end,
-						highlight = function(ctx)
-							return "BlinkCmpLabel"
-						end,
+						highlight = "BlinkCmpSource",
 					},
 				},
 			},
-			selection = "auto_insert",
 		},
 		documentation = {
-			min_width = 10,
-			max_width = math.min(80, vim.o.columns),
-			max_height = 20,
-			border = "rounded",
-			winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
-			direction_priority = {
-				autocomplete_north = { "e", "w", "n", "s" },
-				autocomplete_south = { "e", "w", "s", "n" },
-			},
 			auto_show = true,
 			auto_show_delay_ms = 100,
 			update_delay_ms = 10,
+			window = {
+				max_width = math.min(80, vim.o.columns),
+				border = "rounded",
+			},
 		},
-		signature_help = {
-			min_width = 1,
-			max_width = 100,
-			max_height = 10,
-			border = "rounded",
-			winhighlight = "Normal:BlinkCmpSignatureHelp,FloatBorder:BlinkCmpSignatureHelpBorder",
-		},
-	},
-	kind_icons = {
-		Snippet = "",
 	},
 })
