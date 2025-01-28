f040de657522b163b29eaa04dcd04c455a917f97
WizardStark <juan.barnard10@gmail.com>
2025-01-28 12:20:42 +0200

    Add back git blame line

--- home/.config/nvim/lua/plugins/git.lua
+++ home/.config/nvim/lua/plugins/git.lua
@@ -33,4 +33,14 @@ return {
 		cmd = { "DiffviewOpen", "DiffviewClose" },
 		config = true,
 	},
+	{
+		"f-person/git-blame.nvim",
+		event = "VeryLazy",
+		opts = {
+			enabled = true,
+			message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+			date_format = "%Y-%d-%m",
+			virtual_text_column = 120,
+		},
+	},
 }
