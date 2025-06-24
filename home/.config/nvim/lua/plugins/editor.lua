c8c770f70c932963c49c075e2feaf51ebbdb9f03
WizardStark <juan.barnard10@gmail.com>
2025-06-24 07:35:06 +0200

    Remove old mini.pairs default config

--- home/.config/nvim/lua/plugins/editor.lua
+++ home/.config/nvim/lua/plugins/editor.lua
@@ -27,22 +27,6 @@ return {
 		"echasnovski/mini.pairs",
 		event = { "InsertEnter" },
 		config = true,
-		opts = {
-			mappings = {
-				-- Prevents the action if the cursor is just before any character or next to a "\".
-				["("] = { action = "open", pair = "()", neigh_pattern = "[^\\][%s%)%]%}]" },
-				["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\][%s%)%]%}]" },
-				["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\][%s%)%]%}]" },
-				-- This is default (prevents the action if the cursor is just next to a "\").
-				[")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
-				["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
-				["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
-				-- Prevents the action if the cursor is just before or next to any character.
-				['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^%w][^%w]", register = { cr = false } },
-				["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%w][^%w]", register = { cr = false } },
-				["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^%w][^%w]", register = { cr = false } },
-			},
-		},
 	},
 	{
 		"windwp/nvim-ts-autotag",
