c4f9f4af25d0448f101be5549d7a81029fc1f1ea
WizardStark <juan.barnard10@gmail.com>
2025-06-25 10:47:04 +0200

    Move overseer C-hjkl binds to C-M-hjkl

--- home/.config/nvim/lua/config/terminal/overseer.lua
+++ home/.config/nvim/lua/config/terminal/overseer.lua
@@ -6,6 +6,16 @@ require("overseer").setup(
 			min_height = 25,
 			max_height = 25,
 			default_detail = 1,
+			bindings = {
+				["<C-h>"] = false,
+				["<C-j>"] = false,
+				["<C-k>"] = false,
+				["<C-l>"] = false,
+				["<C-M-l>"] = "IncreaseDetail",
+				["<C-M-h>"] = "DecreaseDetail",
+				["<C-M-k>"] = "ScrollOutputUp",
+				["<C-M-j>"] = "ScrollOutputDown",
+			},
 		},
 	}
 )
