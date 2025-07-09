a261133efe641092237a320a4ecb8acf566ab498
WizardStark <juan.barnard10@gmail.com>
2025-07-09 11:45:44 +0200

    Fix git commit from toggleterm breaking when workspaces are not loaded

--- home/.config/nvim/lua/config/terminal/flatten.lua
+++ home/.config/nvim/lua/config/terminal/flatten.lua
@@ -1,3 +1,5 @@
+local saved_terminal
+
 require("flatten").setup(
 	---@module 'flatten'
 	{
@@ -9,14 +11,32 @@ require("flatten").setup(
 			should_block = function(argv)
 				return vim.tbl_contains(argv, "-b")
 			end,
+			pre_open = function()
+				if not vim.g.workspaces_loaded then
+					local term = require("toggleterm.terminal")
+					local termid = term.get_focused_id()
+					saved_terminal = term.get(termid)
+				end
+			end,
 			post_open = function(ctx)
 				if ctx.is_blocking then
-					require("workspaces.toggleterms").close_visible_terms(true)
+					if vim.g.workspaces_loaded then
+						require("workspaces.toggleterms").close_visible_terms(true)
+					elseif saved_terminal then
+						saved_terminal:close()
+					end
+				else
+					vim.api.nvim_set_current_win(ctx.winnr)
 				end
 			end,
 			block_end = function()
 				vim.schedule(function()
-					require("workspaces.toggleterms").toggle_active_terms(true)
+					if vim.g.workspaces_loaded then
+						require("workspaces.toggleterms").toggle_active_terms(true)
+					elseif saved_terminal then
+						saved_terminal:open()
+						saved_terminal = nil
+					end
 				end)
 			end,
 		},
