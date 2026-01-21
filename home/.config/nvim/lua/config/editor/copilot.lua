586f6e6738ab3a2bc00eeb3119280ff798746281
WizardStark <juan.barnard10@gmail.com>
2026-01-21 14:24:06 +0200

    Only log copilot error messages

--- home/.config/nvim/lua/config/editor/copilot.lua
+++ home/.config/nvim/lua/config/editor/copilot.lua
@@ -1,21 +1,27 @@
-require("copilot").setup({
-	suggestion = {
-		auto_trigger = true,
-		hide_during_completion = false,
-		keymap = {
-			accept = false,
-			accept_word = false,
-			accept_line = false,
-			next = false,
-			prev = false,
-			dismiss = false,
+require("copilot").setup(
+	---@module 'copilot'
+	{
+		logger = {
+			print_log_level = vim.log.levels.ERROR,
 		},
-	},
-	server_opts_overrides = {
-		settings = {
-			telemetry = {
-				telemetryLevel = "off",
+		suggestion = {
+			auto_trigger = true,
+			hide_during_completion = false,
+			keymap = {
+				accept = false,
+				accept_word = false,
+				accept_line = false,
+				next = false,
+				prev = false,
+				dismiss = false,
 			},
 		},
-	},
-})
+		server_opts_overrides = {
+			settings = {
+				telemetry = {
+					telemetryLevel = "off",
+				},
+			},
+		},
+	}
+)
