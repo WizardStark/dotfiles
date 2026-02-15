52a517c64a9d834322a147245eac564d8739f9be
BushMiner <reghardtjooste@live.com>
2026-02-15 16:38:58 +0200

    Add TS injections for mise and add otter for LSPs in mise.toml

--- home/.config/nvim/lua/config/editor/treesitter.lua
+++ home/.config/nvim/lua/config/editor/treesitter.lua
@@ -23,3 +23,9 @@ require("nvim-treesitter-textobjects").setup({
 		include_surrounding_whitespace = false,
 	},
 })
+
+require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
+	local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+	local filename = vim.fn.fnamemodify(filepath, ":t")
+	return string.match(filename, ".*mise.*%.toml$") ~= nil
+end, { force = true, all = false })
--- home/.config/nvim/lua/plugins/editor.lua
+++ home/.config/nvim/lua/plugins/editor.lua
@@ -83,6 +83,21 @@ return {
 			},
 		},
 	},
+	{
+		"jmbuhr/otter.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			vim.api.nvim_create_autocmd({ "FileType" }, {
+				pattern = { "toml" },
+				group = vim.api.nvim_create_augroup("EmbedToml", {}),
+				callback = function()
+					require("otter").activate()
+				end,
+			})
+		end,
+	},
 	-- COMPLETION
 	{
 		"saghen/blink.cmp",
