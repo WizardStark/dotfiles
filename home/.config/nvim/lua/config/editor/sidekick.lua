e70c5084cd7acb64c537b0b338064a82612b63a4
BushMiner <reghardtjooste@live.com>
2026-02-16 17:12:18 +0200

    Add commit prompt

--- home/.config/nvim/lua/config/editor/sidekick.lua
+++ home/.config/nvim/lua/config/editor/sidekick.lua
@@ -4,5 +4,8 @@ require("sidekick").setup({
 			backend = "tmux",
 			enabled = false,
 		},
+		prompts = {
+			commit = "Please update the WIP commit with a more descriptive message, use git log -n 5 to see recent commits for message format. DO NOT use conventional commit format, just a descriptive message is enough. If required add multiline details in the body of the commit message. If there is no WIP commit but there are unstaged changes then create a new commit with a descriptive message. If there are no WIP commits and no unstaged changes then do nothing.",
+		},
 	},
 })
